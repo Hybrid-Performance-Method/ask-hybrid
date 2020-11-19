a8b829bedd0b66e1dc0d78cbc6c5823d53bceae3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-19 15:51:00 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -27,7 +27,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Welcome to Ask Hybrid! How can I help you with your tracking?"
+        speak_output = "Welcome to Hybrid Nutrition! How can we help?"
 
         return (
             handler_input.response_builder
